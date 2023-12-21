dca344e25237361b05d1e94ee99134a263a1bc33
fillord <152933528+fillord@users.noreply.github.com>
2023-12-21 14:20:29 +0600

    Add files via upload

---
+++ calc2/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCalculatorApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) // бесконечном цикл
+            {
+                Console.Clear();
+                double firstValue, secondValue;
+                string action;
+
+                try
+                {
+                    // Ввод первого числа
+                    Console.WriteLine("Введите число 1: ");
+                    firstValue = double.Parse(Console.ReadLine());
+
+                    // Ввод второго числа
+                    Console.WriteLine("Введите число 2: ");
+                    secondValue = double.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    // Обработка ошибки при вводе чисел
+                    Console.WriteLine("Не удалось преобразовать строку в число!");
+                    Console.ReadLine();
+                    continue;
+                }
+
+                // Выбор операции
+                Console.WriteLine("Выберите операцию: '+', '-', '*', '/', '^', 'sqrt'");
+                action = Console.ReadLine();
+
+                if (action == "+")
+                {
+                    Console.WriteLine(firstValue + secondValue);
+                }
+                else if (action == "-")
+                {
+                    Console.WriteLine(firstValue - secondValue);
+                }
+                else if (action == "*")
+                {
+                    Console.WriteLine(firstValue * secondValue);
+                }
+                else if (action == "/")
+                {
+                    // Обработка деления на ноль
+                    if (secondValue == 0)
+                        Console.WriteLine("На 0 делить нельзя!");
+                    else
+                        Console.WriteLine(firstValue / secondValue);
+                }
+                else if (action == "^")
+                {
+                    Console.WriteLine(Math.Pow(firstValue, secondValue));
+                }
+                else if (action == "sqrt")
+                {
+                    if (firstValue < 0)
+                    {
+                        // Обработка извлечения квадратного корня из отрицательного числа
+                        Console.WriteLine("Квадратный корень из отрицательного числа не определен!");
+                        Console.ReadLine();
+                        continue;
+                    }
+                    Console.WriteLine(Math.Sqrt(firstValue));
+                }
+                else
+                {
+                    // Обработка неизвестной операции
+                    Console.WriteLine("Ошибка! Неизветное действие!");
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ calculater/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCalculatorApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) // бесконечном цикл
+            {
+                Console.Clear();
+                double firstValue, secondValue, result;
+                string action;
+
+                try
+                {
+                    // Ввод первого числа
+                    Console.WriteLine("Введите число 1: ");
+                    firstValue = double.Parse(Console.ReadLine());
+
+                    // Ввод второго числа
+                    Console.WriteLine("Введите число 2: ");
+                    secondValue = double.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    // Обработка ошибки при вводе чисел
+                    Console.WriteLine("Не удалось преобразовать строку в число!");
+                    Console.ReadLine();
+                    continue;
+                }
+
+                // Выбор операции
+                Console.WriteLine("Выберите операцию: '+', '-', '*', '/', '^', 'sqrt'");
+                action = Console.ReadLine();
+
+                switch (action)
+                {
+                    case "+":
+                        result = firstValue + secondValue;
+                        break;
+                    case "-":
+                        result = firstValue - secondValue;
+                        break;
+                    case "*":
+                        result = firstValue * secondValue;
+                        break;
+                    case "/":
+                        if (secondValue == 0)
+                        {
+                            // Обработка деления на ноль
+                            Console.WriteLine("На 0 делить нельзя!");
+                            Console.ReadLine();
+                            continue;
+                        }
+                        result = firstValue / secondValue;
+                        break;
+                    case "^":
+                        result = Math.Pow(firstValue, secondValue);
+                        break;
+                    case "sqrt":
+                        if (firstValue < 0)
+                        {
+                            // Обработка извлечения квадратного корня из отрицательного числа
+                            Console.WriteLine("Квадратный корень из отрицательного числа не определен!");
+                            Console.ReadLine();
+                            continue;
+                        }
+                        result = Math.Sqrt(firstValue);
+                        break;
+                    default:
+                        // Обработка неизвестной операции
+                        Console.WriteLine("Ошибка! Неизвестное действие!");
+                        Console.ReadLine();
+                        continue;
+                }
+                // Вывод результата
+                Console.WriteLine("Результат: " + result);
+                Console.ReadLine();
+            }
+        }
+    }
+}
